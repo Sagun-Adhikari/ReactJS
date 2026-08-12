b4462afbe635567c160f362738a2822a489eb454
sagun <adhikarisagun03@gmail.com>
2026-08-12 17:31:37 +0545

    adjusted pages

--- my-app/src/Student.jsx
+++ my-app/src/Student.jsx
@@ -1,6 +1,6 @@
 //props app.jsx line no 400
 function Student(props) {
-  return <h2>Hello Student,{props.name} which have age: {props.age}</h2>;
+  return <h2>Hello Student,{props.name} which  age is : {props.age}</h2>;
 }
 
 export default Student;
--- my-app/src/footer.jsx
+++ my-app/src/footer.jsx
@@ -1,3 +1,13 @@
+// function Footer(){
+//     const name='sagun adhikari';
+//     return (
+// <>
+// <p>this site is made by {name}</p>
+// </>
+//     );
+// }
+// export default Footer;
+
 function Footer(){
     const name='sagun adhikari';
     return (
