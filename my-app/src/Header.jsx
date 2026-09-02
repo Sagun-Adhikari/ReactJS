5341573d813c88b3ea1e5bb29a6e5845a36f11b2
sagun <adhikarisagun03@gmail.com>
2026-09-02 23:35:54 +0545

    added project1 components

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -21,6 +21,15 @@
 // export default Header;
 
 
+//passing  multiple data from app to header
+// function Header(props) {
+//   return <h1>hey this is {props.name} and it is made by {props.madeby}</h1>;
+// }
+
+// export default Header;
+
+
+
 
 
 
